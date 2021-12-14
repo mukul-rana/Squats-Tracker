47bd4e202dc3597db7194aa2c46e4e86e264decc
Mukul <mukulrana617@gmail.com>
2021-12-14 14:17:14 +0530

    Blueprint added for tracker

--- app/__init__.py
+++ app/__init__.py
@@ -26,16 +26,17 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/')
-    def hello():
-        return 'Hello, World!'
+
 
     from . import db
     db.init_app(app)
 
     from . import auth
     app.register_blueprint(auth.bp)
+
+    from . import tracker
+    app.register_blueprint(tracker.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
 
